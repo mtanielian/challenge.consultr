1c152c14b7af9920ddd0d6bcd1cbca622b695e4c
Martin Tanielian <martin.tanielian@gmail.com>
2022-12-23 05:52:46 -0300

    add one test in click image list

--- test/components/ListImages/List.test.js
+++ test/components/ListImages/List.test.js
@@ -1,14 +1,13 @@
 import { act, fireEvent, render, screen } from "@testing-library/react"
 import React from "react"
-import { MemoryRouter, useNavigate } from "react-router-dom"
+import { MemoryRouter } from "react-router-dom"
 import List from "../../../src/components/ListImages/List"
 import { HeroContext } from "../../../src/contexts/HeroeContext"
 import { mockHeroes } from "../../mocks/heroes"
-
-
+import * as router from 'react-router'
 
 describe('Tests component <List />', () => {
-   test('should render component <List /> of images', () => {    
+  test('should render component <List /> of images', () => {    
     render(
       <MemoryRouter>
         <HeroContext.Provider value={{heroes: mockHeroes}}>
@@ -18,4 +17,26 @@ describe('Tests component <List />', () => {
     )
     expect(screen.getAllByRole('img').length).toBe(mockHeroes.length)
   })
+
+  test('should go detail heroe when click image in <List /> of images', () => {    
+    const navigate = jest.fn()
+    jest.spyOn(router, 'useNavigate').mockImplementation(() => navigate)
+    
+    render(
+      <MemoryRouter>
+        <HeroContext.Provider value={{heroes: mockHeroes}}>
+          <List />
+        </HeroContext.Provider>
+      </MemoryRouter>
+      
+    )
+      
+    act(() => {
+      fireEvent.click(screen.getByRole('img', { name: mockHeroes[0].name}))
+    });
+    
+    expect(navigate).toHaveBeenCalled();
+  })
 })
+
+
